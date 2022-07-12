2a57c9c4b09f74915432f90f65cd0f89d15e112e
TefoAM <tefopsalm37@gmail.com>
2022-07-12 23:21:51 +0200

    feat: added fontawesome to angular

--- Peoplesoft-Clone/src/app/home/home.component.ts
+++ Peoplesoft-Clone/src/app/home/home.component.ts
@@ -1,30 +1,69 @@
 import { Component } from '@angular/core';
+import {
+  faWallet,
+  faBuildingColumns,
+  faCalendarDays,
+  faUserGraduate,
+  faClipboard,
+  faPeopleGroup,
+  faListCheck,
+  faArrowsSpin,
+  faCoins,
+  faHouse,
+  faCompass,
+  faUser,
+  faFileLines,
+  faEllipsisVertical,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  constructor() {}
+  faWallet = faWallet;
+  faBuildingColumns = faBuildingColumns;
+  faHouse = faHouse;
+  faEllipsesVertical = faEllipsisVertical;
+  faCompass = faCompass;
+  
 
-  constructor() { }
+  faArr:  any[]= [faWallet,
+    faUser,
+    faUser,
+    faBuildingColumns,
+    faCalendarDays,
+    faUserGraduate,
+    faClipboard,
+    faPeopleGroup,
+    faListCheck,
+    faArrowsSpin,
+    faCoins,
+    faHouse,
+    faCompass,
+    faUser,
+    faFileLines,
+    faEllipsisVertical,
+  ];
 
   tileItems: string[] = [
-    "Allowances",
-    "Profile",
-    "Application Summary",
-    "Admissions",
-    "Academic Records",
-    "Meeting Attendance",
-    "MoU/PPA",
-    "Manage Classes",
-    "Student Finance",
-    "Undergraduate Funding",
-    "Postgrad & Postddoc Funding",
-    "Join & Societies",
-    "Tasks",
-    "Service Requests",
-    "Important Links",
-    "Offers",
+    'Allowances',
+    'Profile',
+    'Application Summary',
+    'Admissions',
+    'Academic Records',
+    'Meeting Attendance',
+    'MoU/PPA',
+    'Manage Classes',
+    'Student Finance',
+    'Undergraduate Funding',
+    'Postgrad & Postddoc Funding',
+    'Join & Societies',
+    'Tasks',
+    'Service Requests',
+    'Important Links',
+    'Offers',
   ];
 }
