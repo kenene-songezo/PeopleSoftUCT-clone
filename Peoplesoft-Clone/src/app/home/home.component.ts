6d135187641f7c6671970f097204062a2f1ab2ba
TefoAM <tefopsalm37@gmail.com>
2022-07-12 20:46:42 +0200

    feat: created the shell for lending page

--- Peoplesoft-Clone/src/app/home/home.component.ts
+++ Peoplesoft-Clone/src/app/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  tileItems: string[] = [
+    "Allowances",
+    "Profile",
+    "Application Summary",
+    "Admissions",
+    "Academic Records",
+    "Meeting Attendance",
+    "MoU/PPA",
+    "Manage Classes",
+    "Student Finance",
+    "Undergraduate Funding",
+    "Postgrad & Postddoc Funding",
+    "Join & Societies",
+    "Tasks",
+    "Service Requests",
+    "Important Links",
+    "Offers",
+  ];
 }
