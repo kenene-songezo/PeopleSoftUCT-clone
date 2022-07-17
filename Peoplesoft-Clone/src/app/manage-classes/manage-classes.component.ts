8092b9e8fe539057ee39dae03bef7dbf1fe1e716
TefoAM <tefopsalm37@gmail.com>
2022-07-18 00:01:15 +0200

    feat: modified the shell and updated the parent components using it

--- Peoplesoft-Clone/src/app/manage-classes/manage-classes.component.ts
+++ Peoplesoft-Clone/src/app/manage-classes/manage-classes.component.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { faCalendarDays, faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { SideNav } from '../types/sideNav.interface';
 
 @Component({
   selector: 'app-manage-classes',
@@ -7,5 +9,19 @@ import { Component} from '@angular/core';
 })
 export class ManageClassesComponent {
 midNav: string = 'View My Classes';
+navList: SideNav[] = [
+  {
+    title: 'View My Classes',
+    icon: faCalendarDays,
+  },
+  {
+    title: 'Shopping cart',
+    icon: faCartShopping,
+  },
+  {
+    title: 'Search Class and Enroll',
+    icon: faMagnifyingGlass,
+  }
+]
 
 }
--- Peoplesoft-Clone/src/app/shell/shell.component.ts
+++ Peoplesoft-Clone/src/app/shell/shell.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { SideNav } from '../types/sideNav.interface';
 
 @Component({
   selector: 'app-shell',
@@ -7,4 +8,6 @@ import { Component, Input } from '@angular/core';
 })
 export class ShellComponent{
   @Input() midNav!: string;
+  @Input() navList!: SideNav[];
+  @Input() hasHeader: boolean = false;
 }
